961509b13a43261ee0ac87c3d8a32a17226a5809
M0hamady <mohammedmohammedy96@gmail.com>
2023-01-22 21:20:09 +0200

    changes

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 
 import Navbar from "components/Navbars/IndexNavbar";
 import Footer from "components/Footers/Footer.js";
