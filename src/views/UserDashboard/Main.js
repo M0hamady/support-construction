ff9862ec8f6f237d7f5236623383110112dd27ab
M0hamady <mohammedmohammedy96@gmail.com>
2023-01-24 20:40:34 +0200

    main

--- src/views/UserDashboard/Main.js
+++ src/views/UserDashboard/Main.js
@@ -1,25 +1,24 @@
 import UseToken from "data/useToken";
 import { useHistory as Usehistory } from "react-router";
-import React from "react";
-import { Switch, Route, Redirect } from "react-router";
+import React, { useState } from "react";
 import Beside from "./Component/Beside";
 import Body from "./Component/Body";
 import Header from "./Component/Header";
 
 export default function main() {
   const { token, setToken } = UseToken();
-  const CheckLogin = () => (token == undefined ? true : false);
+  const [islogin, setislogin] = useState(localStorage.is_login ? true : false);
   const history = Usehistory();
-  if (CheckLogin()) {
+  if (!islogin) {
     history.push("/auth");
   }
   return (
-    <div className="main-dash-user w-screen h-screen bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200">
-      <div className="side-user shadow rounded bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200 ">
+    <div className="main-dash-user w-screen h-screen bg-gradient-to-tr bg-blue-200 ">
+      <div className="side-user shadow rounded  bg-gradient-to-tr bg-blue-200 ">
         <Header />
         <Body />
         <div className="side-user-footer">
-          <h6>@mohammedy  c 2023</h6>
+          <h6>@mohammedy c 2023</h6>
         </div>
       </div>
       <Beside />
